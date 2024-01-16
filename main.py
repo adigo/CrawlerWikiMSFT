9ffaf2ff4feec0e5566a55f00c2b508ee1653e3d
adigo <adigo@nmsu.edu>
2024-01-15 23:35:01 -0600

    Add ini

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Created Date: 01/15/2024
 import re
 from collections import Counter
+from configparser import ConfigParser
 from string import punctuation
 
 import requests
@@ -9,16 +10,26 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = "https://en.wikipedia.org/wiki/Microsoft"
-    return_word_cnt = 10
-    exclusion_list = 'the to and'.split()
+    url = 'https://en.wikipedia.org/wiki/Microsoft'
+    default_config = '''
+    [number_of_words]
+    number = 10
+    
+    [words_to_exclude]
+    string = 
+    '''
+    config = ConfigParser()
+    config.read_string(default_config)
+    config.read('CrawlerWikiMSFT.ini')
+    return_word_cnt = int(config.get('number_of_words', 'number'))
+    exclusion_list = config.get('words_to_exclude', 'string').split(',')
     words = []
 
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
+    soup = BeautifulSoup(response.content, 'html.parser')
     found_history = False
 
-    for c in list(soup.findAll(["h2", "p", "h3"])):
+    for c in list(soup.findAll(['h2', 'p', 'h3'])):
         if c.name == 'h2' and c.text == 'History':
             found_history = True
         elif c.name in ('p', 'h3') and found_history:
